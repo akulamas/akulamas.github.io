30e58da18d6323e7f2f8f8e67fe1fa35b479176c
akulamas <80547701+akulamas@users.noreply.github.com>
2022-04-18 14:05:33 +0300

    Update game.js

--- test GB/game.js
+++ test GB/game.js
@@ -109,7 +109,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('https://akulamas.github.io/test%20GB/end.html')
     }
 
     questionCounter++
